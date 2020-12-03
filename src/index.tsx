32f3b5aac389636109b086a00a8d3476b66da005
Sarunkorn <sirlight.k@gmail.com>
2020-12-03 23:09:15 +0700

    - edit: root directory, add root, error boundary

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import CountryPage from './country_page';
@@ -7,15 +7,23 @@ import { Routes } from './Route';
 
 import './index.scss';
 import { GetRoute } from './helper';
+import { RecoilRoot } from 'recoil';
+import ErrorBoundary from './error_boundary';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Switch>
-        <Route exact path={Routes.Main} component={App} />
-        <Route path={GetRoute(Routes.CountryData, 'name')} component={CountryPage} />
-      </Switch>
-    </BrowserRouter>
+    <ErrorBoundary>
+      <Suspense fallback={<div>Loading...</div>}>
+        <BrowserRouter>
+          <RecoilRoot>
+            <Switch>
+              <Route exact path={Routes.Main} component={App} />
+              <Route path={GetRoute(Routes.CountryData, 'name')} component={CountryPage} />
+            </Switch>
+          </RecoilRoot>
+        </BrowserRouter>
+      </Suspense>
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
