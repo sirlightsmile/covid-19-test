945a6605310790bf0e82d13670929348d3ade3db
Sarunkorn <sirlight.k@gmail.com>
2020-12-04 00:19:41 +0700

    - add: show country data in country page

--- src/country_page.tsx
+++ src/country_page.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { useRecoilValue } from 'recoil';
+import { countryState } from './recoil/country_recoils';
 
 export interface MatchParams {
   name: string;
@@ -8,7 +10,22 @@ export interface MatchParams {
 interface CountryPageProps extends RouteComponentProps<MatchParams> {}
 
 function CountryPage(props: CountryPageProps) {
-  return <div>Country Data of {props.match.params.name}</div>;
+  const countriesMap = useRecoilValue(countryState);
+  const countryName = props.match.params.name;
+  const countryData = countriesMap.get(countryName);
+
+  return countryData ? (
+    <div>
+      <h1>Country Data of {countryData.name}</h1>
+      {Object.entries(countryData).map(([keys, value]) => (
+        <p>
+          {keys} : {value.toString()}
+        </p>
+      ))}
+    </div>
+  ) : (
+    <div>No data.</div>
+  );
 }
 
 export default withRouter(CountryPage);
