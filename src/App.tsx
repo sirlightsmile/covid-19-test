1f2971e2a53ffc34c1111d98a26ef10ed374139a
Sarunkorn <sirlight.k@gmail.com>
2020-12-03 23:18:30 +0700

    - refacetor: move select country logic to recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
 import FlagContainer from './components/flag_container';
-import { CountryData } from './models/country_model';
 import { useRecoilValue } from 'recoil';
 import { countryState } from './recoil/country_recoils';
 import './App.scss';
 
-const SELECTED_COUNTRY_NUMBER = 29;
-const SELECTED_COUNTRY_POPULATION = 300000;
-
 function App() {
-  const countriesData = useRecoilValue(countryState);
-  let selectedCountry: CountryData[] = [];
-
-  for (const data of countriesData) {
-    if (data.population >= SELECTED_COUNTRY_POPULATION) {
-      selectedCountry.push(data);
-    }
-
-    if (selectedCountry.length === SELECTED_COUNTRY_NUMBER) {
-      break;
-    }
-  }
+  const countriesMap = useRecoilValue(countryState);
+  const countriesData = Object.values(countriesMap);
 
   return (
     <div className="App">
@@ -29,7 +15,7 @@ function App() {
       </header>
       <main>
         <h2>Select country to show data</h2>
-        <FlagContainer countryDataList={selectedCountry} />
+        <FlagContainer countryDataList={countriesData} />
       </main>
     </div>
   );
--- src/recoil/country_recoils.ts
+++ src/recoil/country_recoils.ts
@@ -2,11 +2,29 @@ import { selector } from 'recoil';
 import { CountryData } from '../models/country_model';
 import { GetAllCountryRequest } from '../requests/get_all_country_request';
 
-export const countryState = selector<CountryData[]>({
+const SELECTED_COUNTRY_NUMBER = 29;
+const SELECTED_COUNTRY_POPULATION = 300000;
+
+export const countryState = selector<Record<string, CountryData>>({
   key: 'countryState',
   get: async () => {
     try {
-      return new GetAllCountryRequest().start();
+      const countriesData = await new GetAllCountryRequest().start();
+      const selectedCountry: Record<string, CountryData> = {};
+
+      let count = 0;
+      for (const data of countriesData) {
+        if (data.population >= SELECTED_COUNTRY_POPULATION) {
+          selectedCountry[data.name] = data;
+          count++;
+        }
+
+        if (count === SELECTED_COUNTRY_NUMBER) {
+          break;
+        }
+      }
+
+      return selectedCountry;
     } catch (e) {
       throw e;
     }
