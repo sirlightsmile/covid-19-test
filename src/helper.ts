913a207e8c2911441ab43a2806313b142a447b29
Sarunkorn <sirlight.k@gmail.com>
2020-12-03 22:44:42 +0700

    - add: routes helper

---
+++ src/helper.ts
@@ -0,0 +1,19 @@
+import { Routes } from './Route';
+
+/**
+ * Get link with params
+ * @param route base routes
+ * @param params routes parameter
+ */
+export function GetLink(route: Routes, ...params: string[]): string {
+  return route + params.join('/');
+}
+
+/**
+ * Get route with params
+ * @param route base routes
+ * @param params routes parameter
+ */
+export function GetRoute(route: Routes, ...params: string[]): string {
+  return route + ':' + params.join('/:');
+}
