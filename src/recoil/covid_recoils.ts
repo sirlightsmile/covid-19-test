ffbafe95e8bbd3bedc139d46b4c60ecdff8f4539
Sarunkorn <sirlight.k@gmail.com>
2020-12-04 20:15:45 +0700

    - update: covid recoil

--- src/recoil/covid_recoils.ts
+++ src/recoil/covid_recoils.ts
@@ -2,6 +2,7 @@ import { selectorFamily } from 'recoil';
 import * as _ from 'lodash';
 import { CovidData } from '../models/covid_data_model';
 import { GetCovidStatusRequests } from '../requests/get_covid_status_request';
+import { MonthList } from '../config/month';
 
 const FROM_DATE = '2020-01-01T00:00:00Z';
 const TO_DATE = '2020-05-31T00:00:00Z';
@@ -18,8 +19,7 @@ export const covidState = selectorFamily({
 
       let covidData;
       if (res && res.length > 0) {
-        //month, case
-        const caseMap = new Map<number, number>();
+        const caseMap = new Map<MonthList, number>();
         res.forEach((o) => {
           const month = o.Date.getMonth();
           const current = caseMap.get(month) ?? 0;
