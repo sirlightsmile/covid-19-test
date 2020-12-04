ce4f43a76fb6fc41ebf41d516b4ba947db2c8958
Sarunkorn <sirlight.k@gmail.com>
2020-12-04 19:45:35 +0700

    - add: get covid status request

---
+++ src/requests/get_covid_status_request.ts
@@ -0,0 +1,40 @@
+import { BaseAPIConfig, ContentType, HttpMethod } from './base_http_request';
+import { GenericHTTPRequest } from './generic_http_request';
+import { COVID_API_URL } from '../config/config';
+import { ICovidData, isCovidData } from '../models/covid_data_model';
+
+export interface Params {
+  countryName: string;
+  from: Date;
+  to: Date;
+}
+
+/**
+ * Get COVID status from country
+ */
+export class GetCovidStatusRequests extends GenericHTTPRequest<ICovidData[]> {
+  constructor(params: Params) {
+    const config: BaseAPIConfig = {
+      method: HttpMethod.GET,
+      contentType: ContentType.JSON,
+      baseURL: COVID_API_URL,
+      url: `/country/${params.countryName}/`,
+      params: {
+        from: params.from.toDateString(),
+        to: params.to.toDateString(),
+      },
+    };
+    super(config, isGetAllCountryRequest);
+  }
+}
+
+export function isGetAllCountryRequest(obj: unknown): obj is ICovidData[] {
+  const result = obj as ICovidData[];
+
+  const isArray = Array.isArray(result);
+  if (isArray && result.length > 0) {
+    return isCovidData(result[0]);
+  }
+
+  return isArray;
+}
